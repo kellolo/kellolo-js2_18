6ba2ccc686a4e6d55fd23406b6ca9253ff164f5e
Sergey-TR <63286489+Sergey-TR@users.noreply.github.com>
2020-07-07 13:38:22 +0300

    корзина с сервера
    "корзина" так и открывается через детаилс, кстати из видео, которое Вы рекомендовали, "людоедский, или какой то интерфейс, товарищ ведущий лекцию говорил что это хорошо. Допзадание не сделал, пока мозгов не хватает, промучался с выводом тотал сумм и общим количеством, вроде сделал, но чуствую что коряво, наверное надо было как то в отдельную функцию сувать, если можно подскажите.

--- Students/Sergey_Trofimov/project 6_7/server_7/src/components/Basket.vue
+++ Students/Sergey_Trofimov/project 6_7/server_7/src/components/Basket.vue
@@ -1,70 +1,106 @@
 <template>
-    <!-- <div class="basket-block"> --><div>
-        <div class="basket-items">
-            <item v-for="item of items" :key="item.id_product" itemType="basket" :item="item"/>            
-        </div>
+  <!-- <div class="basket-block"> -->
+  <div>
+    <div class="basket-items scrollY">
+      <item v-for="item of items" :key="item.id_product" itemType="basket" :item="item" />
     </div>
+  </div>
 </template>
 
 <script>
-import item from './Item.vue'
+import item from "./Item.vue";
 export default {
-    components: { item },
-    data() {
-        return {
-            items: [],
-            url: 'https://raw.githubusercontent.com/Sergey-TR/testrepo/master/getBasket.json'
-
-        }
-    },
-    methods: {
-        add(item) {
-            
-            let find = this.items.find(el => el.id_product == item.id_product);
-            if (!find) {
-                console.log('Создаю новый товар ' + item.product_name)
-                this.items.push(Object.assign({}, item, {quantity: 1}));
-            } else {
-                console.log('Увеличиваем ' + item.product_name)
-                find.quantity++;
-            }
+  components: { item },
+  data() {
+    return {
+      items: [],
+      url: "/api/basket"
+      // url: 'https://raw.githubusercontent.com/gavrilovem/catalogData/master/getBasket.json'
+    };
+  },
+  methods: {
+    add(item) {
+      let find = this.items.find(el => el.id_product == item.id_product);
+      if (!find) {
+        let newItem = Object.assign({}, item, { quantity: 1 });
+        this.$parent.post(this.url, newItem).then(res => {
+          if (res.status) {
+            this.items.push(newItem);
 
             let totalSumm = 0;
             let mutch = 0;
-       
-            for (let key in this.items) {
-            totalSumm += this.items[key].price * this.items[key].quantity;
-            mutch += this.items[key].quantity;
-       }
-   
-       document.querySelector('.span_total').textContent = totalSumm;
-       document.querySelector('.cart__total_span').textContent = mutch;
 
-        },
-        remove(item) {
-            let find = this.items.find(el => el.id_product == item.id_product);
-            if (find.quantity > 1) {
-                find.quantity--;
-            } else {
-                this.items.splice(this.items.indexOf(find), 1);
+            for (let key in this.items) {
+              totalSumm += this.items[key].price * this.items[key].quantity;
+              mutch += this.items[key].quantity;
             }
 
-            let totalSumm = +document.querySelector('.span_total').textContent;
-            let mutch = +document.querySelector('.cart__total_span').textContent;
-            totalSumm -= find.price;
-            mutch --;
-   
-       document.querySelector('.span_total').textContent = totalSumm;
-       document.querySelector('.cart__total_span').textContent = mutch;
-        },
+            document.querySelector(".span_total").textContent = totalSumm;
+            document.querySelector(".cart__total_span").textContent = mutch;
+          } else {
+            console.log("ERR_ADD_ITEM:" + newItem.product_name);
+          }
+        });
+      } else {
+        this.$parent
+          .put(`/api/basket/${item.id_product}`, { amount: 1 })
+          .then(res => {
+            if (res.status) {
+              find.quantity++;
+
+              let totalSumm = 0;
+              let mutch = 0;
+
+              for (let key in this.items) {
+                totalSumm += this.items[key].price * this.items[key].quantity;
+                mutch += this.items[key].quantity;
+              }
+
+              document.querySelector(".span_total").textContent = totalSumm;
+              document.querySelector(".cart__total_span").textContent = mutch;
+            } else {
+              console.log("ERR_ADD_ITEM:" + item.product_name);
+            }
+          });
+      }
     },
-    mounted() {
-        this.$parent.get(this.url).then(d => this.items = d.contents)
-        
+    remove(item) {
+      let find = this.items.find(el => el.id_product == item.id_product);
+      if (find.quantity > 1) {
+        this.$parent
+          .put(`/api/basket/${item.id_product}`, { amount: -1 })
+          .then(res => {
+            if (res.status) {
+              find.quantity--;
+            } else {
+              console.log("ERR_REMOVE_ITEM:" + item.product_name);
+            }
+          });
+      } else {
+        this.$parent.delete(`/api/basket/${item.id_product}`).then(res => {
+          if (res.status) {
+            this.items.splice(this.items.indexOf(find), 1);
+          } else {
+            console.log("ERR_REMOVE_ITEM:" + item.product_name);
+          }
+        });
+      }
+
+      let totalSumm = +document.querySelector(".span_total").textContent;
+      let mutch = +document.querySelector(".cart__total_span").textContent;
+      totalSumm -= find.price;
+      mutch--;
+
+      document.querySelector(".span_total").textContent = totalSumm;
+      document.querySelector(".cart__total_span").textContent = mutch;
+
     }
-}
+  },
+  mounted() {
+    this.$parent.get(this.url).then(d => (this.items = d.contents));
+  }
+};
 </script>
 
 <style>
-
 </style>
--- Students/Sergey_Trofimov/project 6_7/server_7/src/views/Shop.vue
+++ Students/Sergey_Trofimov/project 6_7/server_7/src/views/Shop.vue
@@ -174,7 +174,34 @@ export default {
         },
         get(url) {
             return fetch(url).then(data => data.json());
-        }
+        },
+        post(url, obj) {
+            return fetch(url, {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(obj)
+            }).then(data => data.json());
+        },
+        put(url, obj) {
+            return fetch(url, {
+                method: 'PUT',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(obj)
+            }).then(data => data.json());
+        },
+        delete(url) {
+            return fetch(url, {
+                method: 'DELETE',
+                headers: { "Content-Type": "application/json" }
+            }).then(data => data.json());
+        },
+        // total(url, obj) {
+        //     return fetch(url, {
+        //         method: 'PUT',
+        //         headers: { "Content-Type": "application/json" },
+        //         body: JSON.stringify(obj)
+        //     }).then(data => data.json());
+        // }
     },
     mounted() {
         
